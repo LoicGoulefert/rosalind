1e59f910b9b3e726365f393c3a0da4bd7eeba8fd
Loic <loic.goulefert31@gmail.com>
2021-01-31 19:53:36 +0100

    Added Rabbits and Recurrence Relations solution

---
+++ Rabbits-and-Recurrence-Relations/rabbits.py
@@ -0,0 +1,15 @@
+def fibonacci_rabbits(n, k):
+    if n == 1 or n == 2:
+        return 1
+
+    return fibonacci_rabbits(n - 1, k) + k * fibonacci_rabbits(n - 2, k)
+
+
+if __name__ == "__main__":
+    path = "rosalind_fib.txt"
+
+    with open(path) as f:
+        data = f.read().strip().split(" ")
+        n, k = int(data[0]), int(data[1])
+
+    print(fibonacci_rabbits(n, k))
