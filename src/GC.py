7207761b24e2cdcaee4142500e7f8f32b1e478cf
Loic <loic.goulefert31@gmail.com>
2021-02-02 17:56:46 +0100

    Refactor common utils functions

--- src/GC.py
+++ src/GC.py
@@ -1,24 +1,4 @@
-def parse_fasta(data):
-    """
-    Args:
-        data: fasta input as a list of lines
-
-    Returns:
-        a dict with key = sequence name and value = sequence
-    """
-    res = {}
-
-    for i, line in enumerate(data):
-        if line[0] == ">":
-            key = line[1:]
-            value = ""
-            for sub_seq in data[i + 1 :]:
-                if sub_seq[0] == ">":
-                    break
-                value += sub_seq
-            res[key] = value
-
-    return res
+from utils import parse_fasta
 
 
 def compute_GC(sequences):
